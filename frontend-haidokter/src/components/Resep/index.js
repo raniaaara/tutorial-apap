72e8cda994438b23cfca0f5aa16e09b3a0b5de16
rania <rania.azzahra@bwa.id>
2020-12-10 22:44:34 +0700

    feat: finish tutorial

---
+++ frontend-haidokter/src/components/Resep/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classes from "./styles.module.css";
+// import Button from "./components/Button";
+import Button from "../Button";
+
+
+const Resep = (props) => {
+    const { noResep, namaDokter, namaPasien, catatan, handleDelete, handleEdit} = props;
+        return (
+        <div className={classes.resep}>
+        <h3>{`Resep Nomor ${noResep}`}</h3>
+        <p>{`Nama Dokter: ${namaDokter}`}</p>
+        <p>{`Nama Pasien: ${namaPasien}`}</p>
+        <p>{`Nama Catatan: ${catatan}`}</p>
+        <Button onClick={handleEdit} variant="success">
+            Edit
+        </Button>
+        <Button onClick={handleDelete} variant="danger">
+            Delete
+        </Button>
+        </div>
+    );
+};
+
+export default Resep;
--- haidokter/src/main/java/apap/tutorial/haidokter/security/WebSecurityConfig.java
+++ haidokter/src/main/java/apap/tutorial/haidokter/security/WebSecurityConfig.java
@@ -11,23 +11,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.
-                authorizeRequests()
+        http
+//                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()).and()
+                .authorizeRequests()
                 .antMatchers("/css/**").permitAll()
                 .antMatchers("/js/**").permitAll()
+                .antMatchers("/api/v1/**").permitAll()
                 .antMatchers("/resep/**").hasAnyAuthority("APOTEKER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login").permitAll()
                 .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll();
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll()
+                .and()
+                .cors()
+                .and()
+                .csrf()
+                .disable();
     }
     @Bean
     public BCryptPasswordEncoder encoder(){
